667b8b4d17aac429626466131de1f70700d02bff
ashishydv701-eng <ashishydv701@gmail.com>
2026-04-22 12:33:07 +0530

    Add files via upload

---
+++ DS1.js
@@ -0,0 +1,67 @@
+let fifoOrder = [];
+let priorityOrder = [];
+
+// Update severity display
+document.getElementById("severity").oninput = function() {
+  document.getElementById("sevValue").innerText = this.value;
+};
+
+// Add Patient
+async function addPatient() {
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  const severity = document.getElementById("severity").value;
+
+  await fetch("http://localhost:18080/add", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({ name, age, severity })
+  });
+
+  loadPatients();
+}
+
+// Load Patients
+async function loadPatients() {
+  const res = await fetch("http://localhost:18080/patients");
+  const data = await res.json();
+
+  const list = document.getElementById("patientList");
+  list.innerHTML = "";
+
+  data.forEach(p => {
+    let li = document.createElement("li");
+
+    if (p.severity <= 3) li.className = "low";
+    else if (p.severity <= 7) li.className = "medium";
+    else li.className = "high";
+
+    li.innerText = `${p.name} (Severity: ${p.severity})`;
+    list.appendChild(li);
+  });
+}
+
+// Treat FIFO
+async function treatFIFO() {
+  const res = await fetch(" /treat/fifo");
+  const data = await res.json();
+
+  fifoOrder.push(data.name);
+  document.getElementById("fifoOrder").innerText = fifoOrder.join(" → ");
+
+  loadPatients();
+}
+
+// Treat Priority
+async function treatPriority() {
+  const res = await fetch("http://localhost:18080/treat/priority");
+  const data = await res.json();
+
+  priorityOrder.push(data.name);
+  document.getElementById("priorityOrder").innerText = priorityOrder.join(" → ");
+
+  loadPatients();
+}
+
+// Initial Load
+loadPatients();
